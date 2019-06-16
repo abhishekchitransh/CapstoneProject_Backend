328cbe47fb1aa3a0d57a686c15988f9634c6c5db
inDiscover <latif002@gmail.com>
2019-06-16 17:48:12 +0530

    Exception handling while updating restaurant details

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/business/RestaurantBusinessService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/business/RestaurantBusinessService.java
@@ -1,10 +1,13 @@
 package com.upgrad.FoodOrderingApp.service.business;
 
 
+import com.upgrad.FoodOrderingApp.service.dao.CustomerDao;
 import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
 import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerAuthTokenEntity;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantCategoryEntity;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.AuthorizationFailedException;
 import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +15,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Service
@@ -25,6 +29,9 @@ public class RestaurantBusinessService {
 
     @Autowired
     private CategoryBusinessService categoryBusinessService;
+
+    @Autowired
+    CustomerDao customerDao;
 
     public List<RestaurantEntity> getAllRestaurants(){
         List<RestaurantEntity> allRestaurants = restaurantDAO.getAllRestaurants();
@@ -51,11 +58,37 @@ public class RestaurantBusinessService {
     }
 
     @Transactional(propagation = Propagation.REQUIRED)
-    public RestaurantEntity updateRestaurantDetails(Integer restaurantId, BigDecimal customerRating, RestaurantEntity restaurantEntity){
-        restaurantEntity.setCustomeRating(customerRating);
-        restaurantEntity.setNumbrOfCustomersRated(restaurantEntity.getNumbrOfCustomersRated()+1);
-        RestaurantEntity updatedRestaurantEntity = restaurantDAO.updateRestaurantDetails(restaurantId,customerRating,restaurantEntity);
-        return updatedRestaurantEntity;
+    public RestaurantEntity updateRestaurantDetails(String authorization ,String restaurantUUID, BigDecimal customerRating) throws AuthorizationFailedException,RestaurantNotFoundException{
+
+        final ZonedDateTime current = ZonedDateTime.now();
+        String [] bearerToken = authorization.split("Bearer ");
+        CustomerAuthTokenEntity customerAuthTokenEntity= customerDao.checkAuthToken(bearerToken[1]);
+
+        if(customerAuthTokenEntity != null && customerAuthTokenEntity.getLogoutAt() != null)
+        {
+            throw new AuthorizationFailedException("ATHR-002","Customer is logged out. Log in again to access this endpoint");
+        }
+
+        if(customerAuthTokenEntity != null && (customerAuthTokenEntity.getExpiresAt().isBefore(current) || customerAuthTokenEntity.getExpiresAt().isEqual(current))){
+            throw new AuthorizationFailedException("ATHR-003" , "Your session is expired. Log in again to access this endpoint");
+        }
+
+        if (customerAuthTokenEntity!=null && customerAuthTokenEntity.getAccessToken().equals(bearerToken[1])) {
+            if(restaurantUUID.equals("")){
+                throw new RestaurantNotFoundException("RNF-002","Restaurant id field should not be empty");
+            }
+            RestaurantEntity restaurantEntity = getRestaurantByUUID(restaurantUUID);
+            if(restaurantEntity == null){
+                throw new RestaurantNotFoundException("RNF-001","No restaurant by this id");
+            }
+            restaurantEntity.setCustomeRating(customerRating);
+            restaurantEntity.setNumbrOfCustomersRated(restaurantEntity.getNumbrOfCustomersRated()+1);
+            RestaurantEntity updatedRestaurantEntity = restaurantDAO.updateRestaurantDetails(restaurantEntity.getId(),customerRating,restaurantEntity);
+            return updatedRestaurantEntity;
+        }
+        else {
+            throw new AuthorizationFailedException("ATHR-001","Customer is not Logged in.");
+        }
     }
 
     public String getCategoriesLinkedToRestaurant(Integer restaurantId){
